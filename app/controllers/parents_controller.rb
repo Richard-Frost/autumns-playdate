ffab309fe7a4b3f2252e323ada1c21477d111527
Richard-Frost <frost.rchrd@gmail.com>
2017-04-28 13:10:23 -0400

    updated views and controllers

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,4 +1,5 @@
 class ParentsController < ApplicationController
+  require 'pry'
 
   get '/parents' do 
     erb :'parents/parents'
@@ -9,12 +10,15 @@ class ParentsController < ApplicationController
   end
 
   post '/parents/signup' do 
-    @parent = Parent.create(name: params[:name], email: params[:email], phone: params[:phone])
-    @child = Child.create(name: params[:name])
+    @parent = Parent.create(name: params[:name], email: params[:email], phone: params[:phone], password: params[:password])
+    @child = Child.create(name: params[:child])
+    Relationship.create(parent_id: @parent.id, child_id: @child.id)
+    session[:id] = @parent.id
     redirect to :"/parents/home"
   end
 
   get '/parents/home' do
+    @parent = Parent.find_by_id(session[:id])
     erb :'/parents/home'
   end
 
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,8 +1,11 @@
 class Parent < ActiveRecord::Base
+
+  has_secure_password
+  
   has_many :relationships
   has_many :children, through: :relationships
   has_many :playdate, through: :child
 
-  validates :name, presence: true
-  validates :email, uniqueness: true
+ validates :name, presence: true
+ validates :email, uniqueness: true
 end
